cd4526aec7128bd1cad4f0238b69fe5095a2fa0b
Dovydas <ulonasd@gmail.com>
2022-12-09 18:53:28 +0200

    Animated backround

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" type="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Darbas</h1>
+    <div id="canvas-shapes"></div>
+<div class="content">
+  <h1>Canvas Shapes</h1>
+  <h2>Change shapes/size as you want</h2>
+</div>  
+<!--You Can Change shapes With "circle" , "square" , "triangle", "hexa" or custom image at Bottom of Js code -->
+</body>
+</html>
